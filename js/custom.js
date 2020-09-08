1befaaf64fff34ec72d0c78f63d752f2119335e9
MikeWhalenIII <michael1327@hotmail.com>
2020-09-08 19:56:45 -0400

    update

--- js/custom.js
+++ js/custom.js
@@ -176,8 +176,8 @@
             var $this = $(this);
             $this.typed({
                 strings: $this.attr('data-elements').split(','),
-                typeSpeed: 150,
-                backDelay: 90000
+                typeSpeed: 100,
+                backDelay: 3000
             });
         });
     },  
